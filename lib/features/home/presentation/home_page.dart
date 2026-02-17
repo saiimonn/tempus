d23a9e661563b05c038667dd003d3966b29682c9
saiimonn <gementizasgg08@gmail.com>
2026-02-17 23:26:03 +0800

    WIP: added first empty card in home

--- lib/features/home/presentation/home_page.dart
+++ lib/features/home/presentation/home_page.dart
@@ -104,8 +104,8 @@ class _HomePageState extends State<HomePage> {
           // PLACEHOLDERS FOR PAGES
           const Center(child: Text("Schedule Page")),
           const Center(child: Text("Tasks Page")),
-          const Center(child: Text("Subjects Page")),
           const Center(child: Text("Finances Page")),
+          const Center(child: Text("Subjects Page")),
         ]
       ),
 
@@ -124,6 +124,94 @@ class _HomePageState extends State<HomePage> {
         children: [
           const SizedBox(height: 20),
           WText("Welcome, $firstName", className: "text-2xl font-bold mb-4"),
+
+          _buildEmptyTaskCard(),
+          const SizedBox(height: 20),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildEmptyTaskCard() {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 20),
+      clipBehavior: Clip.antiAlias,
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.black.withValues(alpha: 0.8),
+            blurRadius: 10,
+            offset: const Offset(0, 4),
+          ),
+        ],
+      ),
+
+      child: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+            decoration: const BoxDecoration(
+              color: AppColors.brandBlue,
+              borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      WText("Today's Tasks", className: "text-sm text-white"),
+                      const SizedBox(height: 5),
+                      WText("Nothing on the plate yet", className: "text-white text-xl font-bold"),
+                    ],
+                  ),
+                ),
+
+                Container (
+                  height: 70,
+                  width: 70, 
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(color: Colors.white.withValues(alpha: 0.3), width: 8),
+                  ),
+                  child: const Center(
+                    child: Icon(Icons.sentiment_satisfied_alt, color: Colors.white, size: 30),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
+          Container(
+            padding: EdgeInsets.all(16),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.vertical(bottom: Radius.circular(20)),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const Text(
+                  "0 tasks remaining",
+                  style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black87),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: AppColors.brandBlue,
+                    foregroundColor: Colors.white,
+                    padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 8),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  child: const Text("Add Task"),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
